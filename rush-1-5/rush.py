0a7af7db36e9336c9f5cc3f4ef52771214eb5cb7
arunrktech <40395995+arunrktech@users.noreply.github.com>
2026-05-13 23:15:21 -0400

    Implement rush function for pattern printing
    Implemented the rush function to print a pattern based on dimensions x and y.

--- rush-1-5/rush.py
+++ rush-1-5/rush.py
@@ -4,5 +4,16 @@ def rush(x, y):
     if x <= 0 or y <= 0:
         print("Invalid size", file=sys.stderr)
         return
-    # TODO: implement
-    pass
+
+    print(f"When calling rush({x}, {y}):")
+
+    for row in range(y):
+        if x == 1 or y == 1:
+            print("B" * x)
+        else:
+            if row == 0:
+                print("A" + "B" * (x - 2) + "C")
+            elif row == y - 1:
+                print("C" + "B" * (x - 2) + "A")
+            else:
+                print("B" + " " * (x - 2) + "B")
