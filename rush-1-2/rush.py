bd0e0daf0628fe66ad437b1ba0904037a7fcf184
arunrktech <40395995+arunrktech@users.noreply.github.com>
2026-05-13 22:40:09 -0400

    Implement rush function for rectangle drawing
    Implemented the rush function to draw a rectangle based on given dimensions.

--- rush-1-2/rush.py
+++ rush-1-2/rush.py
@@ -4,5 +4,18 @@ def rush(x, y):
     if x <= 0 or y <= 0:
         print("Invalid size", file=sys.stderr)
         return
-    # TODO: implement
-    pass
+
+    top    = "/" + "*" * (x - 2) + "\\"
+    bottom = "\\" + "*" * (x - 2) + "/"
+    middle = "*" + " " * (x - 2) + "*"
+    full   = "*" * x
+
+    for row in range(y):
+        if y == 1:
+            print(full)
+        elif row == 0:
+            print(top)
+        elif row == y - 1:
+            print(bottom)
+        else:
+            print(middle)
